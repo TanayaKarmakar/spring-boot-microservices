31295c7e344e72580360e6f3da2f2075901b0ca5
Tanaya Karmakar <tkarmaka@Tanayas-MacBook-Pro.local>
2020-12-24 07:13:47 +0530

    added beer repository

---
+++ mssc-beer-service/src/main/java/com/app/repositories/BeerRepository.java
@@ -0,0 +1,11 @@
+package com.app.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.app.domain.Beer;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
