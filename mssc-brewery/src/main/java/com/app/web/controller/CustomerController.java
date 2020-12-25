a8ee6bc307e048ef06cb03dddca97205077217c0
Tanaya Karmakar <tkarmaka@Tanayas-MacBook-Pro.local>
2020-12-25 06:37:29 +0530

    added exception handler

--- mssc-brewery/src/main/java/com/app/web/controller/CustomerController.java
+++ mssc-brewery/src/main/java/com/app/web/controller/CustomerController.java
@@ -1,11 +1,18 @@
 package com.app.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,35 +29,34 @@ import com.app.web.model.CustomerDto;
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
 	private final CustomerService customerService;
-	
+
 	public CustomerController(CustomerService customerService) {
 		this.customerService = customerService;
 	}
-	
+
 	@GetMapping("/{customerId}")
-	public ResponseEntity<CustomerDto> getCustomer(@PathVariable UUID customerId) {
+	public ResponseEntity<CustomerDto> getCustomer(@NotNull @PathVariable UUID customerId) {
 		return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<HttpHeaders> handlePost(@RequestBody CustomerDto customerDto) {
+	public ResponseEntity<HttpHeaders> handlePost(@NotNull @Valid @RequestBody CustomerDto customerDto) {
 		CustomerDto savedDto = customerService.saveNewCustomer(customerDto);
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Location", "api/v1/beer/" + savedDto.getId().toString());
-		
+
 		return new ResponseEntity<>(headers, HttpStatus.CREATED);
 	}
-	
+
 	@PutMapping("/{customerId}")
-	public ResponseEntity handleUpdate(@PathVariable UUID customerId, @RequestBody CustomerDto customerDto) {
+	public ResponseEntity handleUpdate(@PathVariable @NotNull UUID customerId, @RequestBody @NotNull @Valid CustomerDto customerDto) {
 		customerService.updateCustomer(customerId, customerDto);
-		return new ResponseEntity(HttpStatus.NO_CONTENT);
-	}
-	
-	@DeleteMapping("/{customerId}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void deleteCustomer(@PathVariable UUID customerId) {
-		customerService.deleteCustomer(customerId);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
+	@DeleteMapping("/{customerId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteCustomer(@PathVariable @NotNull UUID customerId) {
+		customerService.deleteCustomer(customerId);
+	}
 }
