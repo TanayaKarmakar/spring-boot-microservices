a7ecac0fe94e66db7b450d71a058b68f85933eb8
Tanaya Karmakar <tkarmaka@Tanayas-MacBook-Pro.local>
2020-12-25 06:40:20 +0530

    removed unused imports

--- mssc-brewery/src/main/java/com/app/web/controller/CustomerController.java
+++ mssc-brewery/src/main/java/com/app/web/controller/CustomerController.java
@@ -1,10 +1,7 @@
 package com.app.web.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
@@ -12,7 +9,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
