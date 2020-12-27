919e57eeafc0e9f02b1f9a8c4adba568367ac5ef
Tanaya Karmakar <tkarmaka@Tanayas-MacBook-Pro.local>
2020-12-27 06:53:51 +0530

    rest docs

--- mssc-beer-service/src/main/java/com/app/web/model/BeerDto.java
+++ mssc-beer-service/src/main/java/com/app/web/model/BeerDto.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,6 +33,7 @@ public class BeerDto {
 	private OffsetDateTime lastModifiedDate;
 	
 	@NotBlank
+	@Size(min = 3, max = 100)
 	private String beerName;
 	
 	@NotNull
@@ -44,5 +46,7 @@ public class BeerDto {
 	@Positive
 	@NotNull
 	private BigDecimal price;
+	
+	@Positive
 	private Integer quantityOnHand;
 }
