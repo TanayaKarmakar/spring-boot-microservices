427997e9d495bbcde5c8727d5635a9bf3c953e63
Tanaya Karmakar <tkarmaka@Tanayas-MacBook-Pro.local>
2020-12-31 06:19:49 +0530

    mssc-beer-service sfg setup

---
+++ mssc-beer-service-master/target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-12-31T06:18:04+0530",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 14.0.2 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+}
--- mssc-beer-service/src/main/java/com/app/web/controller/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.app.web.controller;
-
-public class NotFoundException extends RuntimeException {
-
-	
-
-}
